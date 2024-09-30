810c0d4cdf48a779f03b4e2fdddbe43b25289562
masha.rau@mail.ru <masha.rau@mail.ru>
2024-09-30 14:22:23 +0300

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,44 @@
+public class BankAccount {
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Внесено: " + amount + " рублей");
+        System.out.println("Новый баланс: " + balance + " рублей");
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Снято: " + amount + " рублей");
+            System.out.println("Новый баланс: " + balance + " рублей");
+        } else {
+            System.out.println("Недостаточно средств. Снятие не выполнено.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("12345678", 1000.0);
+
+        account.deposit(500.0);
+        account.withdraw(300.0);
+        account.withdraw(1500.0);
+
+        System.out.println("Номер счета: " + account.getAccountNumber());
+        System.out.println("Баланс: " + account.getBalance() + " рублей");
+    }
+}
