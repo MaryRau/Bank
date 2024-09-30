bf0cee92cea02aa38bd6c4afff3324dd9a8551df
masha.rau@mail.ru <masha.rau@mail.ru>
2024-09-30 14:44:49 +0300

    Готовое задание

---
+++ src/Account.java
@@ -0,0 +1,68 @@
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    // Безаргументный конструктор
+    public Account() {
+        id = 0;
+        balance = 0;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+
+    // Конструктор с id и balance
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * (annualInterestRate / 100) / 12;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Недостаточно средств на счете.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
