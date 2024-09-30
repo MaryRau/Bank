36661853f2af1882a3cbe2e07442b901cf81b04e
masha.rau@mail.ru <masha.rau@mail.ru>
2024-09-30 14:37:07 +0300

    Готовое задание

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,19 +1,19 @@
 public class BankAccount {
-    private double balance;
-    private String accountNumber;
+    private float balance;
+    private int accountNumber;
 
-    public BankAccount(String accountNumber, double initialBalance) {
+    public BankAccount(int accountNumber, float initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
 
-    public void deposit(double amount) {
+    public void deposit(float amount) {
         balance += amount;
         System.out.println("Внесено: " + amount + " рублей");
         System.out.println("Новый баланс: " + balance + " рублей");
     }
 
-    public void withdraw(double amount) {
+    public void withdraw(float amount) {
         if (balance >= amount) {
             balance -= amount;
             System.out.println("Снято: " + amount + " рублей");
@@ -28,16 +28,16 @@ public class BankAccount {
         return balance;
     }
 
-    public String getAccountNumber() {
+    public int getAccountNumber() {
         return accountNumber;
     }
 
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("12345678", 1000.0);
+        BankAccount account = new BankAccount(12345678, 1000.0f);
 
-        account.deposit(500.0);
-        account.withdraw(300.0);
-        account.withdraw(1500.0);
+        account.deposit(500.0f);
+        account.withdraw(300.0f);
+        account.withdraw(1500.0f);
 
         System.out.println("Номер счета: " + account.getAccountNumber());
         System.out.println("Баланс: " + account.getBalance() + " рублей");
